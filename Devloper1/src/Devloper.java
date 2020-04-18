235f907ae09afcb6ae79873dd6276b10e7db6217
Maruti Jamdade <maruti.3003@gail.com>
2020-04-18 20:39:59 +0530

    commit after pull by dev 2

--- Devloper1/src/Devloper.java
+++ Devloper1/src/Devloper.java
@@ -4,6 +4,7 @@ public class Devloper {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //one line added by devloper 1
+		//second line added by devloper 2 by pulling project
 	}
 
 }
